0060abceac7a7fabae5c3721fd58e60dbb8ad96e
nicvac-teach <nicola.vacca@marconibari.edu.it>
2024-11-04 19:07:07 +0000

    Session Lesson

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name; // Legge il cookie "name"
+    if (name) {
+        // Se il cookie esiste, mostra la pagina di saluto
+        res.render('greet', { message:'Bentornato', name: name });
+    } else {
+        // Se non esiste, mostra il form
+        res.render('form');
+    }
 });
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    // Imposta un cookie chiamato "name" con valore l'input dell'utente
+    res.cookie('name', name, { maxAge: 24 * 60 * 60 * 1000 }); 
+    res.render('greet', { message:'Benvenuto', name: name });
+});
+
+app.post('/logout', (req, res) => {
+
+    //Cancella tutti i cookies
+    Object.keys(req.cookies).forEach(cookie => res.clearCookie(cookie));
+
+    res.redirect('/'); // Reindirizza alla home page
 });
 
 const PORT = 3000;
